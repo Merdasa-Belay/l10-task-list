aa1ee7b16ca2477e4c442392c8f3bcabb6ff5cfe
Merdasa-Belay <merdasabelay@gmail.com>
2024-09-02 21:39:32 +0300

    update

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -4,6 +4,8 @@
 
 @section('content')
 
+    {{ $errors }}
+
     <form method="POST" action="{{ route('tasks.store') }}">
 
         @csrf
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Symfony\Component\HttpFoundation\Response;
@@ -32,5 +33,17 @@ Route::get('/tasks/{id}', function ($id) {
 
 
 Route::post('/tasks', function (Request $request) {
-    dd(request()->all());
+    // dd(request()->all());
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required'
+
+    ]);
+    $task = new Task;
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+    $task->save();
+    return redirect()->route('tasks.show', ['id' => $task->id])->with('success', '');
 })->name('tasks.store');
