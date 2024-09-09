345823e6a467603d420510c496e4c0ac0a738cf0
Merdasa-Belay <merdasabelay@gmail.com>
2024-09-09 19:14:39 +0300

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,11 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel 11 Task List App</title>
+    {{-- Fontawsome --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    {{-- Bootstrap --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
     @yield('styles')
 </head>
 
 <body>
-    <h1>
+    <h1 class="container-fluid d-grid align-items-center justify-content-center">
         @yield('title')
     </h1>
     <div>
@@ -24,5 +30,9 @@
     </div>
 
 </body>
+{{-- Bootstrap --}}
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/js/bootstrap.min.js"></script>
+
 
 </html>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,51 +1,61 @@
 @extends('layouts.app')
 
 @section('title', 'The list of tasks')
+
 @section('content')
 
+    <div class="container-fluid d-grid align-items-center justify-content-center">
+        <div class="mb-4">
+            <a href="{{ route('tasks.index') }}" class="font-weight-bold text-gray-700 text-decoration-underline text-danger">
+                <i class="fa fa-arrow-circle-left"></i> Go back to the task list!
+            </a>
+        </div>
 
+        <p class="mb-4 text-gray-700">
+            {{ $task->description }}
+        </p>
 
-    <p>
-        {{ $task->description }}
-    </p>
-
-
-    <p>
-        {{ $task->long_description }}
-    </p>
-    <p>
-        {{ $task->created_at }}
-    </p>
-    <p>
-        {{ $task->updated_at }}
-
-    </p>
-    <p>
-        @if ($task->completed)
-            completed
-        @else
-            Not completed
+        @if ($task->long_description)
+            <p class="mb-4 text-gray-700">
+                {{ $task->long_description }}
+            </p>
         @endif
-    </p>
-    <div>
-        <form method="POST" action="{{ route('tasks.toggle-complete', ['task' => $task]) }}">
 
-            @csrf
-            @method('PUT')
-            <button type="submit">
-                Mark as {{ $task->completed ? 'not completed' : 'completed' }}
-            </button>
-        </form>
-    </div>
-    <div>
-        <a href="{{ route('tasks.edit', ['task' => $task]) }}">edit</a>
+        <p class="mb-4 text-sm text-muted">Created
+            {{ $task->created_at->diffForHumans() }} • Updated {{ $task->updated_at->diffForHumans() }}
+        </p>
+
+        <p class="mb-4">
+            @if ($task->completed)
+                <span class="mb-4" style="color: green;font-size: medium;font-weight: 500">Completed</span>
+            @else
+                <span class="mb-4" style="color: red;font-size: medium;font-weight: 500">Not completed</span>
+            @endif
+        </p>
+
+        <div class="d-flex gap-2">
+            <div>
+                <form method="POST" action="{{ route('tasks.toggle-complete', ['task' => $task]) }}">
+                    @csrf
+                    @method('PUT')
+                    <button type="submit" class="btn btn-primary">
+                        Mark as {{ $task->completed ? 'not completed' : 'completed' }}
+                    </button>
+                </form>
+            </div>
+
+            <div>
+                <a href="{{ route('tasks.edit', ['task' => $task]) }}" class="btn btn-warning">Edit</a>
+            </div>
+
+            <div>
+                <form action="{{ route('tasks.destroy', ['task' => $task]) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </div>
+        </div>
     </div>
 
-    <div>
-        <form action="{{ route('tasks.destroy', ['task' => $task]) }}" method="POST">
-            @csrf
-            @method('DELETE')
-            <button type="submit">delete</button>
-        </form>
-    </div>
 @endsection
